d5f865bd128012d3b9bfb015216b65576ac33602
Qixing Lin <3077006505@qq.com>
2020-10-26 15:26:40 +0800

    feat:规定文件夹命名

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-  </div>
+  <div id="nav"></div>
   <router-view />
 </template>
 
