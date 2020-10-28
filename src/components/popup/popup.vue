52e583b35b6f67e313f1585233e0a7830bf3f8bb
Qixing Lin <3077006505@qq.com>
2020-10-28 19:12:45 +0800

    feat:微信登录

---
+++ src/components/popup/popup.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div class="popup"></div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  data () {
+    return {
+
+    }
+  }
+}
+</script>
+<style lang='scss' scoped>
+.popup {
+  width: 500px;
+  height: 700px;
+  background-color: burlywood;
+}
+</style>
---
+++ src/views/Login.vue
@@ -0,0 +1,57 @@
+
+<template>
+  <div>
+    <button @click="login">点击参与</button>
+    <div :class="show ? 'show' : 'hidden'">
+      <popup></popup>
+    </div>
+  </div>
+</template>
+
+<script>
+import popup from '../components/popup/popup'
+export default {
+  components: {
+    popup
+  },
+  data () {
+    return {
+      num: 0,
+      show: true
+    }
+  },
+  methods: {
+    login () {
+      console.log(1)
+      this.show = !this.show
+    }
+  }
+}
+
+</script>
+<style lang='scss' scoped>
+.show {
+  opacity: 1;
+  animation: show 0.4s linear;
+}
+.hidden {
+  opacity: 0;
+  animation: hidden 0.4s linear;
+}
+@keyframes show {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+  }
+}
+@keyframes hidden {
+  0% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0;
+  }
+}
+</style>
