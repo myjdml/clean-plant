11b3083b1ccb27c84d2a0bb2b5b2957c6273f856
Qixing Lin <3077006505@qq.com>
2020-11-20 17:46:06 +0800

    feat:更新获奖名单页

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from '../views/Home.vue'
-
+import index from '../views/index.vue'
 const routes = [
   {
     path: '/',
@@ -18,7 +18,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/index.vue')
+    // 优先加载
+    component: index
   },
   {
     path: '/push-card',
