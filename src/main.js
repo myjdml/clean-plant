31f185da58611f75dd183a8762af059377765e3e
myjdml <myjdml_codemaster@163.com>
2020-11-20 20:58:20 +0800

    fix：calender

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: 毛神 yyds
  * @Date: 2020-10-26 09:39:44
- * @LastEditTime: 2020-11-20 19:57:43
+ * @LastEditTime: 2020-11-20 20:58:03
  * @LastEditors: Code_Master
  * @FilePath: /clean-plant/src/main.js
  * @Description: The first 90% of the code accounts for the first 90% of the development time. The remaining 10% of the code accounts for the other 90% of the development time.
@@ -14,8 +14,8 @@ import store from './store'
 import 'normalize.css'
 import './assets/style/reset.css'
 import './assets/style/font.css'
-// import VConsole from 'vconsole'
+import VConsole from 'vconsole'
 import { List } from 'vant'
 /* eslint-disable no-unused-vars */
 createApp(App).use(store).use(router).use(List).mount('#app')
-// const vConsole = new VConsole() // 初始化
+const vConsole = new VConsole() // 初始化
