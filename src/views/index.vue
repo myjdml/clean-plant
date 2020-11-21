0177fd2753546193b98a2e6d0ac4fa395e73eebc
kying-star <3077006505@qq.com>
2020-11-22 00:27:45 +0800

    fix:修改日期BUG

--- src/views/index.vue
+++ src/views/index.vue
@@ -293,7 +293,7 @@ export default {
     const week = dayjs.unix(dayjs().unix()).$W
     console.log('这周是')
     this.daylist.forEach((e, index) => {
-      this.daylist[index].num = dayjs().add(index - week, 'day').$D + 1
+      this.daylist[index].num = dayjs().add(index - week, 'day').$D + 1 - 7
       console.log(this.daylist[index].num)
     })
     /**
