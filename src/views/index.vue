0bcd8856aa56e94b2bfeb84569beebdd5cbf257c
kying-star <3077006505@qq.com>
2020-11-23 15:56:00 +0800

    fix:修改打卡日期

--- src/views/index.vue
+++ src/views/index.vue
@@ -304,7 +304,7 @@ export default {
     const week = dayjs.unix(dayjs().unix()).$W
     console.log('这周是')
     this.daylist.forEach((e, index) => {
-      this.daylist[index].num = dayjs().add(index - week, 'day').$D + 1 - 7
+      this.daylist[index].num = dayjs().add(index - week, 'day').$D + 1
       console.log(this.daylist[index].num)
     })
     /**
