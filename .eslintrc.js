a1248131bfdc8b925c3ed7f0c69b2fd6bd1663b9
myjdml <898762316@qq.com>
2021-09-16 14:00:02 +0800

    fix: 首屏空白

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }]
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
 }
