a4760e018228a78731539f5d5ccd6efc7aa0bd7c
myjdml <898762316@qq.com>
2021-09-16 13:59:52 +0800

    fix: 首屏空白

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   productionSourceMap: false,
   publicPath:
         process.env.NODE_ENV === 'production'
-          ? './clean-plant/'
+          ? '/clean-plant/'
           : '/',
   outputDir: 'build',
   devServer: {
